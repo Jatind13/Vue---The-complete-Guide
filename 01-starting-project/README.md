e234f1ed4d08afdefeac7e6b35a8295dc54ebcd9
Jatind13 <jatindixit1305@gmail.com>
2024-07-16 13:50:10 +0530

    Vue js implementation

--- 01-starting-project/README.md
+++ 01-starting-project/README.md
@@ -1,3 +1,10 @@
 This project implements a functionality of to do list just by using the plain javascript .
 
 Now in the next set of tasks we will switch to Vue and see what all things it makes easy to implement it.
+
+Using the Vue in the project using the CDN which we are going to add in the script tag : 
+
+```<script src="https://unpkg.com/vue@3.4.9/dist/vue.global.js"></script> ```
+
+And this is the documentation link : https://vuejs.org/guide/quick-start.html
+
--- 01-starting-project/app.js
+++ 01-starting-project/app.js
@@ -1,15 +1,34 @@
+/* ------------ Declarative Approach --------------- */
+
+Vue.createApp({
+    data(){
+        return {
+            goals:[],
+            enteredValue:""
+        };
+    },
+    methods: {
+        addGoal(){
+            console.log("This is true")
+            this.goals.push(this.enteredValue)
+        }
+    }
+}).mount("#app")
+
+
+/* ------------------Imperative Approach---------------------- */
+
 // Implementaion using the pure Javascript.
+// const buttonEle = document.querySelector('button')
+// const inputEle = document.querySelector('input')
+// const listEle = document.querySelector('ul')
 
-const buttonEle = document.querySelector('button')
-const inputEle = document.querySelector('input')
-const listEle = document.querySelector('ul')
+// function addGoal(){
+//     const enteredValue = inputEle.value;
+//     const listItem = document.createElement('li');
+//     listItem.textContent = enteredValue;
+//     listEle.appendChild(listItem);
+//     inputEle.value = ''
+// }
 
-function addGoal(){
-    const enteredValue = inputEle.value;
-    const listItem = document.createElement('li');
-    listItem.textContent = enteredValue;
-    listEle.appendChild(listItem);
-    inputEle.value = ''
-}
-
-buttonEle.addEventListener('click', addGoal);
+// buttonEle.addEventListener('click', addGoal);
