94fd38b5940c43e2f188bf70bf9aa9fbc1ff1a0f
Jatind13 <jatindixit1305@gmail.com>
2024-07-26 12:14:07 +0530

    basics covered part 3

---
+++ Basics and Core Concepts/basics-01-starting-code/Summary.md
@@ -0,0 +1,9 @@
+First Summary :
+
+- We have learned how to create a Vue app and mount it using the unique name or say id.
+- Secondly the parent elements which is mounted with Vue all the child elements of it will be by default the part of vue then.
+- Then we learned about interpolation {{courseGoal}}
+- Data binding using the directives .
+- Also learned about using the methods in Vue and working with them.
+- Learned about the 'this' keyword and how does it works.
+- Also how to output and html value is what we have learned.
--- Basics and Core Concepts/basics-01-starting-code/app.js
+++ Basics and Core Concepts/basics-01-starting-code/app.js
@@ -15,6 +15,10 @@ const app = Vue.createApp({
     data(){
         return{
             courseGoal:"Learn Vue with Jatin!",
+            //now let's learn how to handle the data in the Vue app.
+            courseGoalA: "Master all the the topics in vue",
+            courseGoalB: "Become a good developer",
+            courseGoalC: '<h2>Good course it is </h2>',
             vueLink:"https://vuejs.org/"
         }
     },
@@ -22,10 +26,12 @@ const app = Vue.createApp({
         outputGoal(){
             const randomNum = Math.random();
             if(randomNum < 0.5){
-                return "Learn Vue"
+                // now here we will use data property using the the 'this' keyword which is nothing 
+                // but more of a core of Vue.
+                return `Learn Vue and ${this.courseGoalA}`
             } 
             else {
-                return "Master Vue"
+                return `Master Vue and ${this.courseGoalB}`
             }
         }
     }
@@ -33,3 +39,8 @@ const app = Vue.createApp({
 
 // step two is mounting or connecting the vue app.
 app.mount("#user-goal");
+
+/* 
+- In the context of a Vue component, this refers to the Vue instance itself. This allows you to access the properties and methods defined within the component.d
+
+*/
