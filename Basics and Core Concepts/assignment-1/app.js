1a48d735354e65fdfabb4e08c7633e1df59181ce
Jatind13 <jatindixit1305@gmail.com>
2024-07-26 12:28:34 +0530

    assignment 1 completed

---
+++ Basics and Core Concepts/assignment-1/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+   data(){
+    return {
+        name : "Jatin Dixit",
+        age : 23,
+        imageUrl : 'https://imgs.search.brave.com/3gJ0ZXF-aq1GVaCLwOiNP0-rJpDbSmh2ep0GizHwsDY/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1YvdnVlanMt/bG9nby0xN0Q1ODZC/NTg3LXNlZWtsb2dv/LmNvbS5wbmc'
+    }
+   },
+})
+
+app.mount("#assignment")
